a167338beec99123ff18c94c48e634692281c1fd
Mark Sibly <Mark Sibly>
2016-05-27 17:06:13 +1200

    Rebuilt binaries.

--- src/common.sh
+++ src/common.sh
@@ -7,12 +7,12 @@ ted2_new=""
 if [ "$OSTYPE" = "linux-gnu" ]
 then
 	mx2cc="../bin/mx2cc_linux"
-	mx2cc_new="mx2new/mx2cc.buildv006/desktop_release_linux/mx2cc"
+	mx2cc_new="mx2new/mx2cc.buildv008/desktop_release_linux/mx2cc"
 	ted2="../bin/ted2_linux"
-	ted2_new="ted2/ted2.buildv006/desktop_release_linux/ted2"
+	ted2_new="ted2/ted2.buildv008/desktop_release_linux/ted2"
 else
 	mx2cc="../bin/mx2cc_macos"
-	mx2cc_new="mx2new/mx2cc.buildv006/desktop_release_macos/mx2cc.app/Contents/MacOS/mx2cc"
+	mx2cc_new="mx2new/mx2cc.buildv008/desktop_release_macos/mx2cc.app/Contents/MacOS/mx2cc"
 	ted2="../bin/ted2.app"
-	ted2_new="ted2/ted2.buildv006/desktop_release_macos/ted2.app"
+	ted2_new="ted2/ted2.buildv008/desktop_release_macos/ted2.app"
 fi
