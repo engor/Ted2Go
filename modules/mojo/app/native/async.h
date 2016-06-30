77019783d9b58f88fb2e757ae73a127ddc60b507
Mark Sibly <blitzmunter@gmail.com>
2016-06-30 12:51:48 +1200

    Added native app/async files.

---
+++ modules/mojo/app/native/async.h
@@ -0,0 +1,17 @@
+
+#ifndef BB_MOJO_APP_ASYNC_H
+#define BB_MOJO_APP_ASYNC_H
+
+#include <bbmonkey.h>
+
+//Call on GUI thread only!
+//
+int bbAddAsyncCallback( bbFunction<void()> callback );
+
+//Can call on any thread...queued for later execution by GUI thread.
+//
+void bbInvokeAsyncCallback( int callback,bool remove );
+
+void bbRemoveAsyncCallback( int callback );
+
+#endif
