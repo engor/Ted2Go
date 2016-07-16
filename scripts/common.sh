8c4c232f974fa1fc885c7b3d6b98fd63035f8c07
Mark Sibly <blitzmunter@gmail.com>
2016-07-17 11:29:23 +1200

    Updated binaries to v1.0.2

--- scripts/common.sh
+++ scripts/common.sh
@@ -10,22 +10,22 @@ if [ "$OSTYPE" = "linux-gnu" ]
 then
 
 	mx2cc="../bin/mx2cc_linux"
-	mx2cc_new="../src/mx2cc/mx2cc.buildv1.0.1/desktop_release_linux/mx2cc"
+	mx2cc_new="../src/mx2cc/mx2cc.buildv1.0.2/desktop_release_linux/mx2cc"
 	
 	ted2="../bin/ted2_linux"
-	ted2_new="../src/ted2/ted2.buildv1.0.1/desktop_release_linux"
+	ted2_new="../src/ted2/ted2.buildv1.0.2/desktop_release_linux"
 	
 	launcher="../Monkey2 (Linux)"
-	launcher_new="../src/launcher/launcher.buildv1.0.1/desktop_release_linux/launcher"
+	launcher_new="../src/launcher/launcher.buildv1.0.2/desktop_release_linux/launcher"
 	
 else
 
 	mx2cc="../bin/mx2cc_macos"
-	mx2cc_new="../src/mx2cc/mx2cc.buildv1.0.1/desktop_release_macos/mx2cc"
+	mx2cc_new="../src/mx2cc/mx2cc.buildv1.0.2/desktop_release_macos/mx2cc"
 	
 	ted2="../bin/ted2_macos.app"
-	ted2_new="../src/ted2/ted2.buildv1.0.1/desktop_release_macos/ted2.app"
+	ted2_new="../src/ted2/ted2.buildv1.0.2/desktop_release_macos/ted2.app"
 	
 	launcher="../Monkey2 (Macos).app"
-	launcher_new="../src/launcher/launcher.buildv1.0.1/desktop_release_macos/launcher.app"
+	launcher_new="../src/launcher/launcher.buildv1.0.2/desktop_release_macos/launcher.app"
 fi
