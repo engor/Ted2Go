3665ca25be00fd373a9317496f601fffd63b5c68
Mark Sibly <blitzmunter@gmail.com>
2016-09-05 11:11:04 +1200

    Updated android template project/readme.

---
+++ android/Monkey2Game/app/src/main/Monkey2Game.java
@@ -0,0 +1,29 @@
+
+package ${PACKAGE_NAME};
+
+import org.libsdl.app.SDLActivity;
+
+import android.os.*;
+import android.util.*;
+import android.content.res.Configuration;
+
+import java.io.*;
+import java.nio.*;
+import java.net.*;
+import java.util.*;
+import java.text.*;
+import java.lang.reflect.*;
+
+/*
+ * A sample wrapper class that just calls SDLActivity
+ */
+public class ${ACTIVITY_NAME} extends SDLActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate( savedInstanceState );
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+}
