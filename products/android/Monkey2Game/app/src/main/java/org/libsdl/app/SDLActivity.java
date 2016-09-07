67854403f76dd56c495a3cc13a9c7cb6ef7fbebc
Mark Sibly <blitzmunter@gmail.com>
2016-09-07 19:50:02 +1200

    Moved product templates.

--- products/android/Monkey2Game/app/src/main/java/org/libsdl/app/SDLActivity.java
+++ products/android/Monkey2Game/app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -70,13 +70,8 @@ public class SDLActivity extends Activity {
      */
     protected String[] getLibraries() {
         return new String[] {
-                "SDL2",
-                // "SDL2_image",
-                // "SDL2_mixer",
-                // "SDL2_net",
-                // "SDL2_ttf",
-                "openal",
-                "mx2_main"
+				"mx2_main",
+				"openal"
         };
     }
 
