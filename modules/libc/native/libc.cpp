632fd41521b49d61a7ed5fe5aefc46bf51a3e5b3
Mark Sibly <blitzmunter@gmail.com>
2016-09-09 20:18:15 +1200

    Tweaks to windows libc.system to prevent consoles popping up.

--- modules/libc/native/libc.cpp
+++ modules/libc/native/libc.cpp
@@ -24,29 +24,20 @@ int system_( const char *cmd ){
 
 #if _WIN32
 
-	bbString tmp;
-	
-	DWORD flags=0;
 	bool inherit=false;
-	PROCESS_INFORMATION pi={0};
+	DWORD flags=CREATE_NO_WINDOW;
 	STARTUPINFOA si={sizeof(si)};
+	PROCESS_INFORMATION pi={0};
+	
+	bbString tmp=BB_T( "cmd /S /C\"" )+BB_T( cmd )+BB_T( "\"" );
 	
 	if( GetStdHandle( STD_OUTPUT_HANDLE ) ){
 	
-		tmp=BB_T( "cmd /S /C\"" )+BB_T( cmd )+BB_T( "\"" );
-	
 		inherit=true;
-		
 		si.dwFlags=STARTF_USESTDHANDLES;
 		si.hStdInput=GetStdHandle( STD_INPUT_HANDLE );
 		si.hStdOutput=GetStdHandle( STD_OUTPUT_HANDLE );
 		si.hStdError=GetStdHandle( STD_ERROR_HANDLE );
-		
-	}else{
-
-		tmp=cmd;
-
-		flags=CREATE_NO_WINDOW;
 	}
 	
 	if( !CreateProcessA( 0,(LPSTR)tmp.c_str(),0,0,inherit,flags,0,0,&si,&pi ) ) return -1;
