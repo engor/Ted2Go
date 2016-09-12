b5ffb92c500ffd3fb93b4cc627a3f004130eea64
Mark Sibly <blitzmunter@gmail.com>
2016-09-13 09:06:57 +1200

    Updated binaries to v1.0.6.

--- scripts/common.bat
+++ scripts/common.bat
@@ -1,9 +1,9 @@
 
 set mx2cc=..\bin\mx2cc_windows.exe
-set mx2cc_new=..\src\mx2cc\mx2cc.buildv1.0.5\windows_release\mx2cc.exe
+set mx2cc_new=..\src\mx2cc\mx2cc.buildv1.0.6\windows_release\mx2cc.exe
 
 set ted2=..\bin\ted2_windows
-set ted2_new=..\src\ted2\ted2.buildv1.0.5\windows_release
+set ted2_new=..\src\ted2\ted2.buildv1.0.6\windows_release
 
 set launcher="..\Monkey2 (Windows).exe"
-set launcher_new=..\src\launcher\launcher.buildv1.0.5\windows_release\launcher.exe
+set launcher_new=..\src\launcher\launcher.buildv1.0.6\windows_release\launcher.exe
