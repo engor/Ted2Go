1c7f0de9860e635cbd73828e63ac3bde98c60748
Mark Sibly <blitzmunter@gmail.com>
2016-10-31 17:16:38 +1300

    Updated binaries.

--- scripts/common.bat
+++ scripts/common.bat
@@ -1,10 +1,10 @@
 
 set mx2cc=..\bin\mx2cc_windows.exe
-set mx2cc_new=..\src\mx2cc\mx2cc.buildv1.0.9\windows_release\mx2cc.exe
-set mx2cc_raspbian_new=..\src\mx2cc\mx2cc.buildv1.0.9\raspbian_release\mx2cc
+set mx2cc_new=..\src\mx2cc\mx2cc.buildv1.0.91\windows_release\mx2cc.exe
+set mx2cc_raspbian_new=..\src\mx2cc\mx2cc.buildv1.0.91\raspbian_release\mx2cc
 
 set ted2=..\bin\ted2_windows
-set ted2_new=..\src\ted2\ted2.buildv1.0.9\windows_release
+set ted2_new=..\src\ted2\ted2.buildv1.0.91\windows_release
 
 set launcher="..\Monkey2 (Windows).exe"
-set launcher_new=..\src\launcher\launcher.buildv1.0.9\windows_release\launcher.exe
+set launcher_new=..\src\launcher\launcher.buildv1.0.91\windows_release\launcher.exe
